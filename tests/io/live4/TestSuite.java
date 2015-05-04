f0c5f29ec7fbec6628604856b26c28367e630323
Vadym Rudenko <vadym.rudenko@gmail.com>
2015-05-04 12:04:14 -0700

    adding test suite

---
+++ tests/io/live4/TestSuite.java
@@ -0,0 +1,14 @@
+package io.live4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	StartPageTest.class,
+	ExplorePageTest.class
+})
+
+public class TestSuite{
+	
+}
